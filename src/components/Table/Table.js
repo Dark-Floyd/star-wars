8a95a826d1b3506b2e36a8c017e04012b1783a63
Zohar Zaborski <204124382@idf.il>
2021-12-21 17:16:12 +0200

    update

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Card } from "react-bootstrap";
+import { Card,Spinner } from "react-bootstrap";
 import classes from "./Table.module.css";
 const Table = () => {
-  const [vehiclez, setVehicle] = useState("");
+  const [vehicleSum, setVehicleSum] = useState("");
   const [max, setMax] = useState(0);
   const [pilots, setPilots] = useState([]);
   const [planets, setPlanets] = useState([]);
+  const [fetchData, setFetchData] = useState(false);
 
   const findHighestSum = async () => {
     let maxSum = max;
@@ -26,14 +27,13 @@ const Table = () => {
           //console.log(homeWorld);
           //console.log(homeWorld.population);
           tempSum += parseInt(homeWorld.population);
-
           if (tempSum > maxSum) {
             maxSum = tempSum;
-            setVehicle(vehicle.name);
+            setVehicleSum(vehicle.name);
             setPlanets([...planets, homeWorld.name, homeWorld.population]);
-            setPilots([...pilots, p.data.name]);
+            setPilots([...pilots, p.name]);
             console.log("The vehcile with max num is " + vehicle.name);
-            console.log(p.data.name);
+            console.log(p.name);
             console.log(planets);
             console.log(homeWorld.name);
             console.log(homeWorld.population);
@@ -44,7 +44,7 @@ const Table = () => {
       }
       setMax(maxSum);
       //console.log(maxSum);
-      //console.log(planets);
+      console.log(planets);
     } catch (error) {
       console.log(error);
     }
@@ -55,13 +55,17 @@ const Table = () => {
   }, [max]);
 
   return (
+
     <Card className={classes.table}>
+      {/* <Spinner animation="border" role="status" style={{margin:'1rem'}}>
+        <span className="visually-hidden">Loading...</span>
+      </Spinner> */}
       <Card.Body>
         <Card.Title>Part I</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           Vehicle name with the largest sum
         </Card.Subtitle>
-        <Card.Text>{vehiclez}</Card.Text>
+        <Card.Text>{vehicleSum}</Card.Text>
         <Card.Subtitle className="mb-2 text-muted">
           Related home planets and their respective population
         </Card.Subtitle>
@@ -73,9 +77,12 @@ const Table = () => {
         <Card.Subtitle className="mb-2 text-muted">
           Related pilot names
         </Card.Subtitle>
-        <Card.Text> {pilots.map((i) => {
+        <Card.Text>
+          {" "}
+          {pilots.map((i) => {
             return <li>{i}</li>;
-          })}</Card.Text>
+          })}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
